e9dce2808c38a7dacf46f3333f477cd215c8c7ee
vadneyk <kentarovadney@berkeley.edu>
2025-02-28 13:29:33 -0800

    Fix class name in zos_monitoring.py

--- zos_ml_demo/utils/zos_monitoring.py
+++ zos_ml_demo/utils/zos_monitoring.py
@@ -8,7 +8,7 @@ import logging
 import psutil
 from datetime import datetime
 
-class ZOSMonitor:
+class SystemMonitor:
     def __init__(self, config):
         self.config = config
         self.logger = logging.getLogger('zos_monitor')
